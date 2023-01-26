ed2e6b15167e1b437de387b6279910de11d2c44c
sametckilic <84988439+sametckilic@users.noreply.github.com>
2023-01-27 02:27:50 +0300

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+
 // Register
 
 router.post("/register", async (req, res) => {
@@ -7,7 +9,7 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
 
     })
 
@@ -17,6 +19,32 @@ router.post("/register", async (req, res) => {
     }
     catch (err) {
         res.status(500).json(err);
+    }
+})
+
+
+router.post('/login', async (req, res) => {
+    try {
+
+        const user = await User.findOne({ username: req.body.username });
+
+        !user && res.status(401).json("Wrong crendtials");
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+
+        const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+        
+        password !== req.body.password && res.status(401).json("Wrong crendials");
+
+        res.status(200).json(user);
+    }
+
+    catch (err) {
+        res.status(500).json(err);
+
     }
 })
 
