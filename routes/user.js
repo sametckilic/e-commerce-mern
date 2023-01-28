8ca1cf465c4af9efd3911a925585881218085fe8
sametckilic <84988439+sametckilic@users.noreply.github.com>
2023-01-29 02:59:46 +0300

    user requests finished

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const { verifyToken, verifyTokenAndAuth, verifyTokenAndAdmin } = require('./veri
 const User = require('../models/User');
 const router = require('express').Router();
 
+// UPDATE USER
+
 router.put("/:id", verifyTokenAndAuth, async (req, res) => {
     if (req.body.password) {
         req.body.password = CryptoJS.AES.encrypt(req.body.password,
@@ -25,10 +27,12 @@ router.put("/:id", verifyTokenAndAuth, async (req, res) => {
 );
 
 
+//DELETE USER
+
 router.delete("/:id", verifyTokenAndAuth, async (req, res) => {
     try {
         const deletedUser = await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted.", deletedUser);
+        res.status(200).json({message: "User has been deleted.", ...deletedUser._doc});
     }
     catch (err) {
         res.status(500).json(err);
@@ -36,16 +40,81 @@ router.delete("/:id", verifyTokenAndAuth, async (req, res) => {
 
 });
 
-router.get("/:id", verifyTokenAndAdmin, async (req,res)=>{
+
+//FIND USER
+
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req,res)=>{
     try{
         const user = await User.findById(req.params.id);
-        res.status(200).json(user);
+        const {password, ...params} = user._doc;
+        res.status(200).json(params);
     }
     catch (err) {
         res.status(500).json(err);
     }
 });
 
+//GET ALL USERS
+
+router.get("/", verifyTokenAndAdmin, async (req,res)=>{
+    
+    const query = req.query.new;
+    
+    try{
+        const users = query 
+        ? await User.find().sort({_id:-1}).limit(1) 
+        : await User.find();
+
+        const params = users.map(user =>{
+            const {password, ...rest} = user._doc
+            return rest;
+        });
+        res.status(200).json(params);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET USER STATS
+
+router.get("/stats", verifyTokenAndAdmin, async (req,res)=>{
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+    try{
+        const data = await User.aggregate([
+            {$match:{
+                createdAt: 
+                {
+                    $gte: lastYear
+                }
+            }},
+            {
+                $project:
+                {
+                    month:
+                    {
+                        $month: "$createdAt"
+                    }
+                }
+            },
+            {
+                $group:
+                {
+                    _id: "$month",
+                    total : {$sum: 1}
+                }
+            }
+        ]);
+
+        res.status(200).json(data);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+
+});
 
 
 
